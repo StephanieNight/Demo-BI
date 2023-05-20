d549c53aa203f45cc31c7ad40521dddaee76a429
Stephanie Night <s.hvenegaard@outlook.com>
2023-05-20 13:32:25 +0200

    added base function with json converter

---
+++ Demo-BI/Backend/Functions/BaseFunction.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace BackendAPI.Functions
+{
+    public class BaseFunction
+    {
+        internal T JSONToObject<T>(string json) where T : class
+        {
+            T value = JsonConvert.DeserializeObject<T>(json);
+            return value;
+        }
+    }
+}
