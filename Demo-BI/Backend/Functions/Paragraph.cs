aa6097c9de1928d243452d609d5246752cb714da
Stephanie Night <s.hvenegaard@outlook.com>
2023-05-20 12:32:47 +0200

    moved. renamed. and sorted usings

--- Demo-BI/Backend/Functions/Paragraph.cs
+++ Demo-BI/Backend/Functions/Paragraph.cs
@@ -1,20 +1,19 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
 
-namespace Backend
+namespace Functions
 {
-    public static class Function1
+    public static class Paragraph
     {
-        [FunctionName("Function1")]
+        [FunctionName("Paragraph")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
