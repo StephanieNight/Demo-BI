d755e7eb1f0ca6aadbc215763f6031877ee2fc96
Stephanie Night <s.hvenegaard@outlook.com>
2023-05-20 17:57:00 +0200

    fixed dependency

--- Demo-BI/Backend/Startup.cs
+++ Demo-BI/Backend/Startup.cs
@@ -1,7 +1,12 @@
-﻿using DataAccess;
+﻿using DataService;
+using DataService.Interfaces;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 
+[assembly: FunctionsStartup(typeof(BackendAPI.Startup))]
 namespace BackendAPI
 {
     public class Startup : FunctionsStartup
@@ -9,8 +14,15 @@ namespace BackendAPI
         public override void Configure(IFunctionsHostBuilder builder)
         {
             var services = builder.Services;
+
+            // == Add universials == 
             services.AddDatabase();
             services.AddLogging();
+
+            // == Services == 
+            services.AddTransient<IDataService, BIDataService>();
+
+            // == Builds provider ==
             services.BuildServiceProvider();
         }
     }
