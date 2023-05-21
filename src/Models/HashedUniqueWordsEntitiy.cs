151850d26a6010b9c2f58cb313f7720a7729d1de
Stephanie Night <s.hvenegaard@outlook.com>
2023-05-21 13:52:36 +0200

    added migration

---
+++ src/Models/HashedUniqueWordsEntitiy.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+
+namespace Models
+{
+    public class HashedUniqueWordsEntitiy : BaseEntity<Guid>
+    {
+        public int Count { get; set; }
+        public int Hash { get; set; }
+        public string Words { get; set; }
+    }
+}
