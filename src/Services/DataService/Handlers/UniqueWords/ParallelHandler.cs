fdab2a9a26f54d8fc2553679f866e6e42c100e41
Stephanie Night <s.hvenegaard@outlook.com>
2023-05-21 12:58:01 +0200

    added the parallel handler and service.

---
+++ src/Services/DataService/Handlers/UniqueWords/ParallelHandler.cs
@@ -0,0 +1,32 @@
+﻿using DataService.Interfaces;
+using System.Collections.Concurrent;
+using System.Reflection.Metadata.Ecma335;
+
+namespace DataService.Handlers.UniqueWords
+{
+    public class ParallelHandler : IUniqueWordsHandler
+    {
+        public string[] GetUniqueWords(string data)
+        {
+            var words = data.Split(new char[] { ' ', '\r', '\n' });
+            var Dictionary = new ConcurrentDictionary<string, int>();
+
+            Parallel.For(0, words.Length, index =>
+            {
+                var word = words[index];
+                if (String.IsNullOrEmpty(word) == false)
+                {
+                    if (Dictionary.ContainsKey(word))
+                    {
+                        Dictionary[word]++;
+                    }
+                    else
+                    {
+                        Dictionary.AddOrUpdate(word, 1, (key, oldValue) => oldValue + 1);
+                    }
+                }
+            });
+            return Dictionary.Keys.ToArray();
+        }
+    }
+}
