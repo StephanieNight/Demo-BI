707ea9b435d1d2119373634566f70148f42c304e
Stephanie Night <s.hvenegaard@outlook.com>
2023-05-21 13:19:05 +0200

    added paragraph

---
+++ src/Services/DataService/Handlers/UniqueWords/ParagraphHandler.cs
@@ -0,0 +1,32 @@
+﻿using DataService.Interfaces;
+using System.Collections.Concurrent;
+
+namespace DataService.Handlers.UniqueWords
+{
+    public class ParagraphHandler : IUniqueWordsHandler
+    {
+        public string[] GetUniqueWords(string data)
+        {
+            var paragraphs = data.Split("\r\n\r\n");
+            
+            var Dictionary = new ConcurrentDictionary<string, int>();
+
+            Parallel.For(0, paragraphs.Length, index =>
+            {
+                var paragraph = paragraphs[index];
+                if (String.IsNullOrEmpty(paragraph) == false)
+                {
+                    var words = paragraph.Split(Constants.Chars);
+                    foreach (var word in words)
+                    {
+                        if (String.IsNullOrEmpty(word) == false)
+                        {
+                            Dictionary.AddOrUpdate(word, 1, (key, oldValue) => oldValue + 1);
+                        }
+                    }
+                }
+            });
+            return Dictionary.Keys.ToArray();
+        }
+    }
+}
